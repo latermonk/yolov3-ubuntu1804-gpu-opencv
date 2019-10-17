97e1824743122b14819655200d3e36e5888c2fc4
latermonk <iback.china@gmail.com>
2019-10-17 14:29:20 +0800

    Update install-opencv.sh

--- install-opencv.sh
+++ install-opencv.sh
@@ -6,7 +6,7 @@
 # -------------------------------------------------------------------- |
 #                       SCRIPT OPTIONS                                 |
 # ---------------------------------------------------------------------|
-OPENCV_VERSION='4.1.1'       # Version to be installed
+OPENCV_VERSION='4.1.0'       # Version to be installed
 OPENCV_CONTRIB='NO'          # Install OpenCV's extra modules (YES/NO)
 # -------------------------------------------------------------------- |
 
